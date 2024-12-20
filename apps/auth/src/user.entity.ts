db635c0de9867aef649cd38bf3833778f1341330
webcreation-dev <adjilan2403@gmail.com>
2024-12-20 09:11:48 -0800

    fix bugs

---
+++ apps/auth/src/user.entity.ts
@@ -0,0 +1,57 @@
+import { Exclude } from 'class-transformer';
+// import { Order } from 'orders/entities/order.entity';
+// import { Property } from 'properties/entities/property.entity';
+import {
+  Column,
+  Entity,
+  // OneToMany,
+  PrimaryGeneratedColumn,
+  // ManyToMany,
+  // JoinTable,
+} from 'typeorm';
+import { RegistryDates } from '@app/common';
+import { Role } from './roles/enums/role.enum';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  // @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({ unique: true })
+  phone: string;
+
+  @Exclude()
+  @Column()
+  password: string;
+
+  @Column({
+    type: 'enum',
+    enum: Role,
+    enumName: 'role_enum',
+    default: Role.ADMIN,
+  })
+  role: Role;
+
+  @Column(() => RegistryDates, { prefix: false })
+  registryDates: RegistryDates;
+
+  // @OneToMany(() => Order, (order) => order.customer, {
+  //   cascade: ['soft-remove', 'recover'],
+  // })
+  // orders: Order[];
+
+  // @ManyToMany(() => Property)
+  // @JoinTable()
+  // wishlist: Property[];
+
+  // @OneToMany(() => Property, (property) => property.owner)
+  // properties: Property[];
+
+  get isDeleted() {
+    return !!this.registryDates.deletedAt;
+  }
+}
